483bc3af67de6fc3ee49c64ec327066027022727
syjoosy <64636699+syjoosy@users.noreply.github.com>
2020-08-17 23:55:56 +0300

    add scroll animation

--- script.js
+++ script.js
@@ -1,85 +1,43 @@
-// console.log("123")
-// class StickyNavigation {
-	
-// 	constructor() {
-// 		this.currentId = null;
-// 		this.currentTab = null;
-// 		this.tabContainerHeight = 70;
-// 		let self = this;
-// 		$('.et-hero-tab').click(function() { 
-// 			self.onTabClick(event, $(this)); 
-// 		});
-// 		$(window).scroll(() => { this.onScroll(); });
-// 		$(window).resize(() => { this.onResize(); });
-// 	}
-	
-// 	onTabClick(event, element) {
-// 		event.preventDefault();
-// 		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
-// 		$('html, body').animate({ scrollTop: scrollTop }, 600);
-// 	}
-	
-// 	onScroll() {
-// 		this.checkTabContainerPosition();
-//     this.findCurrentTabSelector();
-// 	}
-	
-// 	onResize() {
-// 		if(this.currentId) {
-// 			this.setSliderCss();
-// 		}
-// 	}
-	
-// 	checkTabContainerPosition() {
-// 		let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
-// 		if($(window).scrollTop() > offset) {
-// 			$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
-// 		} 
-// 		else {
-// 			$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
-// 		}
-// 	}
-	
-// 	findCurrentTabSelector(element) {
-// 		let newCurrentId;
-// 		let newCurrentTab;
-// 		let self = this;
-// 		$('.et-hero-tab').each(function() {
-// 			let id = $(this).attr('href');
-// 			let offsetTop = $(id).offset().top - self.tabContainerHeight;
-// 			let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
-// 			if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
-// 				newCurrentId = id;
-// 				newCurrentTab = $(this);
-// 			}
-// 		});
-// 		if(this.currentId != newCurrentId || this.currentId === null) {
-// 			this.currentId = newCurrentId;
-// 			this.currentTab = newCurrentTab;
-// 			this.setSliderCss();
-// 		}
-// 	}
-	
-// 	setSliderCss() {
-// 		let width = 0;
-// 		let left = 0;
-// 		if(this.currentTab) {
-// 			width = this.currentTab.css('width');
-// 			left = this.currentTab.offset().left;
-// 		}
-// 		$('.et-hero-tab-slider').css('width', width);
-// 		$('.et-hero-tab-slider').css('left', left);
-// 	}
-	
-// }
-
-// new StickyNavigation();
-
 $(window).scroll(function() {
   var winHeight = $(window).height();
   var docHeight = $(document).height();
   var winScrolled = $(window).scrollTop();
   var docScrolled = $(document).scrollTop();
   var scrollPercent = 160 * winScrolled / (docHeight - winHeight);
-  $('#main').css('width', scrollPercent /1.05 + "%");
+  var main = $('#main').css('width', scrollPercent /1.1);
+  $('#main').css('width', scrollPercent /1.1 + "%");
+});
+
+$(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        });
+  });
+
+// var target = $('.trio');
+// var targetPos = target.offset().top;
+// var winHeight = $(window).height();
+// var scrollToElem = targetPos - winHeight;
+// $(window).scroll(function(){
+//   var winScrollTop = $(this).scrollTop();
+//   if(winScrollTop > scrollToElem){
+//     //сработает когда пользователь доскроллит к элементу с классом .elem
+//     $('#body').css('background', red);
+//   }
+// });
+
+$(document).ready(function() {
+	var windowHeight = $(window).height();
+ 
+	$(document).on('scroll', function() {
+		$('.trio').each(function() {
+			var self = $(this),
+			height = self.offset().top + self.height();
+			if ($(document).scrollTop() + windowHeight >= height) {
+				self.addClass('green')
+			}
+		});
+	});
 });
